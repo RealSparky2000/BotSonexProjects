e5fd5f0b72b2fc56064b05e41849bfaaa783bdce
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-28 19:16:01 +0300

    Update index (1).js

--- index (1).js
+++ index (1).js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const moment = require("moment");
 const client = new Discord.Client();
 client.on('ready', () => {
     client.user.setUsername('Mante')
@@ -8,10 +9,26 @@ client.on('ready', () => {
 
 var prefix = '+';
 client.on('message', message => {
-     if (!message.content.startsWith(prefix)) return;
-    if(message.content === "Hello?") {
-
+    var args = message.content.substring(prefix.length).split(" ");
+    if(!message.content.startsWith(prefix)) return;
+    if(message.content === `${prefix}Hello?`){
          message.channel.send("Hi!");
+      }
+        if(message.content === `${prefix}Goodbye!`){
+         message.channel.send("Bye, Bye!");
+      }
+      if(message.content === `${prefix}info`){
+            var user = message.mentions.users.first() || message.author;
+            var embed = new Discord.RichEmbed()
+                .setThumbnail(user.avatarURL)
+                .setColor("#42f4b0")
+                .addField('ID', user.id, true)
+                .addField('Username', user.username, true)
+                .addField('Discriminator', user.discriminator, true)
+                .addField('Status', user.presence.status, true)
+                .addField('Bot?', user.bot, true)
+                .addField('Joined At', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+            message.channel.send({ embed });
       }
 });
 client.login("NDUwNjMzMDc5NTkwMDkyODEw.De2U3A.vNfQYKF9JYMtcFtYhxUhDlHCWTo");
